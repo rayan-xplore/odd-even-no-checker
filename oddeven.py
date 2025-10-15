a8d29183e3600c76df1962c28315360cc797dca7
Rayan Rathore <rayanwissen1523@gmail.com>
2025-10-15 06:30:11 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+from math import remainder
+
+print('welcome to odd even checker')
+user_no=float(input("what is the no. thta you want to check? "))
+if user_no % 2 == 0:
+    print("your no is : even")
+else:
+    print("your no is : odd")
