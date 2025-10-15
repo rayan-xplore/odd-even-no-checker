69f6dd225660e4db9a590c5ab15105ea603234d3
Rayan Rathore <rayanwissen1523@gmail.com>
2025-10-15 06:43:26 +0530

    oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,8 @@
 print("welcome to odd even checker!")
-user_no=float(input("what is the no. thta you want to check? "))
+user_no=float(input("what is the no. that you want to check? "))
 if user_no % 2 == 0:
     print("your no is : even")
 else:
     print("your no is : odd")
+
 
